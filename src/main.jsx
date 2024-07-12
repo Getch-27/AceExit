fff80e039b13c153b778e7e4fb6db1b239eaad54
Liya Hbtemariam <liyahabtemariam11@gmail.com>
2024-07-12 12:00:03 -0700

    initial commit

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import InstitutionRegistration from './pages/systemAdmin/institutionRegistration'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <InstitutionRegistration />
+  </React.StrictMode>,
+)
---
+++ src/pages/systemAdmin/institutionRegistration.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+function InstitutionRegistration() {
+  return (
+    <> 
+      <h1>Hi systemAdmin</h1>
+      <p className="read-the-docs font-extrabold">
+        You can register institutions here!
+      </p>
+    </>
+  )
+}
+
+export default InstitutionRegistration
