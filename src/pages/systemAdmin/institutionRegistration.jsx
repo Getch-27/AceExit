1d0cd5d7fade804c8586a896b7d1aa3efeee6863
Liya Hbtemariam <liyahabtemariam11@gmail.com>
2024-07-12 13:02:58 -0700

    chore: Added custom colors.

--- src/pages/systemAdmin/institutionRegistration.jsx
+++ src/pages/systemAdmin/institutionRegistration.jsx
@@ -3,7 +3,13 @@ function InstitutionRegistration() {
   return (
     <> 
       <h1>Hi systemAdmin</h1>
-      <p className="read-the-docs font-extrabold">
+      <p className=" text-customPurple">
+        You can register institutions here!
+      </p>
+      <p className=" text-customYellow">
+        You can register institutions here!
+      </p>
+      <p className=" text-customBlue">
         You can register institutions here!
       </p>
     </>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,19 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        customPurple: '#5a6acf',
+        customLightPurple: '#8593ed',
+        customBlue: '#37b1cd',
+        customLightBlue: '#c7ceff',
+        customYellow: '#faa645',
+        customWhite: '#f1f2f7',
+        customGray: '#bfc5d8',
+
+        // Add more custom colors as needed
+    },
   },
   plugins: [],
 }
-
+}
