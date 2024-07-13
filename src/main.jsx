e82818e6680ef20bb413d6fc6fb49fa23ae7e7b9
Liya Hbtemariam <liyahabtemariam11@gmail.com>
2024-07-12 17:15:51 -0700

    refactor: changed the naming of the main component

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import InstitutionRegistration from './pages/systemAdmin/institutionRegistration'
+import SystemAdmin from './pages/systemAdmin'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <InstitutionRegistration />
+    <SystemAdmin />
   </React.StrictMode>,
 )
