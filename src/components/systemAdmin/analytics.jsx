69e6d4af880dbde9ba377d3ac15450b2771c346c
Liya Hbtemariam <liyahabtemariam11@gmail.com>
2024-07-12 17:16:47 -0700

    feat: new analytics component

---
+++ src/components/systemAdmin/analytics.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+const Analytics = () => {
+    return (
+        <div className="bg-customWhite shadow-md rounded-lg p-8">
+            <h2 className="text-2xl font-bold mb-4">Analytics</h2>
+        </div>
+    );
+};
+
+export default Analytics;
