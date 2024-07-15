ea317039e4d867166677c41a71722daecbeedb9c
Getch-27 <getahunlast@gmail.com>
2024-07-15 20:25:15 +0300

    try

--- src/components/common/sidebar.jsx
+++ src/components/common/sidebar.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Sidebar = ({ title, options, onOptionClick, activeOption }) => {
     return (
         <div className=" w-64 bg-customWhite text-customPurple h-full fixed left-0 top-0 p-6 font-poppins">
-            <h2 className="text-l font-bold mb-10 text-center">{title}</h2>
+            <h2 className="text-l font-bold mb-10 text-center ">{title}</h2>
             <ul className="space-y-4">
                 {options.map((option, index) => (
                     <li key={index} 
